ca3e6ce8b60e65b3cc63bf5ffe1621be53ed8844
Sukanya.Das4 <Sukanya.Das4@Cognizant.com>
2021-07-08 14:44:17 +0530

    CQRS

--- eStockMarket/StocksAPI/Models/Stock.cs
+++ eStockMarket/StocksAPI/Models/Stock.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace StocksAPI.Models
 {
-    public class Stock : BaseAggregateRoot<Stock, Guid>
+    public class Stock 
     {
         [BsonId]
         public int StockId { get; set; }
@@ -21,40 +21,5 @@ namespace StocksAPI.Models
         [BsonIgnore]
         [JsonIgnore]
         public virtual Company Company { get; set; }
-
-        public Stock(Guid id, int stockId, decimal stockPrice, string companyCode) : base(id)
-        {
-            if (stockId <= 0)
-                throw new ArgumentOutOfRangeException(nameof(stockId));
-            if (stockPrice <= 0)
-                throw new ArgumentOutOfRangeException(nameof(stockPrice));
-            if (string.IsNullOrWhiteSpace(companyCode))
-                throw new ArgumentOutOfRangeException(nameof(companyCode));
-
-            StockId = stockId;
-            StockPrice = stockPrice;
-            CompanyCode = companyCode;
-            StockDateTime = DateTime.Now;
-
-            this.AddEvent(new StockAdded(this));
-        }
-        protected override void Apply(IDomainEvent<Guid> @event)
-        {
-            switch (@event)
-            {
-                case StockAdded s:
-                    this.Id = s.AggregateId;
-                    this.StockId = s.stockId;
-                    this.StockPrice = s.stockPrice;
-                    this.StockDateTime = s.StockDateTime;
-                    this.CompanyCode = s.companyCode;
-                    break;
-            }
-        }
-
-        public static Stock Add(int stockId, decimal stockPrice, string companyCode)
-        {
-            return new Stock(Guid.NewGuid(), stockId, stockPrice, companyCode);
-        }
     }
 }
